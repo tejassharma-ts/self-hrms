a456228344bfab9b1fc10aad9b5573d8c5951d18
Deepankershamra1999 <Deepankersharma06@gmail.com>
2024-08-31 09:59:53 +0530

    calendar created leave

---
+++ app/(dashboard)/leave/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Calender from './_components/Calender'
+
+const page = () => {
+  return (
+    <div className='h-screen w-full'>
+      <Calender/>
+    </div>
+  )
+}
+
+export default page
